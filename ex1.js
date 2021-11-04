c374b782365c00cce8b83a000171a8ea10d2cb1f
josan <josanescod@gmail.com>
2021-11-04 12:06:43 +0100

    ex1

--- ex1.js
+++ ex1.js
@@ -1,10 +1,11 @@
+"use strict";
 const readLine = require("readline-sync");
 /*
 Given a number return its multiplication table completely
 */
 
 function multiTable(numb) {
-  for (i = 1; i <= 10; i++) {
+  for (let i = 1; i <= 10; i++) {
     console.log(`${i} x ${numb} = ${i * numb}`);
   }
 }
